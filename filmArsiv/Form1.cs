bd02df0a90c8849f71bce103f085e678f1300255
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2024-03-19 13:32:34 +0300

    Add files via upload

---
+++ filmArsiv/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace filmArsiv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Server =localhost; Database=film_arsiv;Uid = root; Pwd='';";
+
+        void VeriGetir()
+        {
+            using (MySqlConnection baglanti = new MySqlConnection(connectionString))
+            {
+                baglanti.Open();
+                string sorgu = "SELECT * FROM filmler";
+                MySqlCommand cmd = new MySqlCommand(sorgu, baglanti);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+
+                DataTable tablo = new DataTable();
+
+                adapter.Fill(tablo);
+
+                dgvFilmler.DataSource = tablo;
+            }
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VeriGetir();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormElestiriler formElestiriler= new FormElestiriler();
+            formElestiriler.ShowDialog();
+        }
+    }
+}
---
+++ filmArsiv/FormElestiriler.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace filmArsiv
+{
+    public partial class FormElestiriler : Form
+    {
+        public FormElestiriler()
+        {
+            InitializeComponent();
+        }
+
+        string connectionString = "Server =localhost; Database=film_arsiv;Uid = root; Pwd='';";
+
+        void VeriGetir()
+        {
+            using (MySqlConnection baglanti = new MySqlConnection(connectionString))
+            {
+                baglanti.Open();
+                string sorgu = "SELECT filmler.film_ad,elestiriler.elestiri_metin,elestiriler.puan,elestiriler.elestiri_tarihi,elestiriler.elestirmen\r\nFROM elestiriler\r\nJOIN filmler ON filmler.film_id=elestiriler.elestiri_id";
+
+                MySqlCommand cmd = new MySqlCommand(sorgu, baglanti);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+
+                DataTable tablo = new DataTable();
+
+                adapter.Fill(tablo);
+
+                dgvElestiriler.DataSource = tablo;
+            }
+        }
+
+        private void FormElestiriler_Load(object sender, EventArgs e)
+        {
+            VeriGetir();
+        }
+    }
+}
